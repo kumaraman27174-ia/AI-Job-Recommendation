665eba2ad11bf823751666eece6a6b35cfa95a8a
Aman Kumar <kumaraman27174@gmail.com>
2026-05-10 21:16:21 +0530

    db.py

--- db.py
+++ db.py
@@ -3,8 +3,9 @@ import mysql.connector
 
 def get_db():
     return mysql.connector.connect(
-        host=os.environ.get("mysql-21048be6-kumaraman27174-6f67.b.aivencloud.com"),
-        user=os.environ.get("avnadmin"),
-        password=os.environ.get("0987654321"),
-        database=os.environ.get("job_ai")
+        host=os.environ.get("MYSQL_HOST"),
+        user=os.environ.get("MYSQL_USER"),
+        port=int(os.environ.get("MYSQL_PORT", 3306)),
+        password=os.environ.get("MYSQL_PASSWORD"),
+        database=os.environ.get("MYSQL_DB")
     )
